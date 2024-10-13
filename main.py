5041298f4bf49fd64a3856572f8df8c3a7786233
SweatyBytes <maxvheiningen@gmail.com>
2024-10-13 21:33:49 +0200

    Added FPS / Delta time

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.time.Clock
+    dt = 0
 
     while 1 == 1:
         screen.fill((0, 0, 0)) # Fill the screen with black
@@ -18,6 +23,9 @@ def main():
         for event in pygame.event.get(): # Iterate over all events in the pygame event queue
             if event.type == pygame.QUIT: # Check if the event is of type QUIT (like clicking 'X' button)
                 return # Exit the main game loop, effectively closing the window
+        
+        pygame.time.Clock.tick(60) # Pause the game loop until 1/60th of a second has passed.
+        dt = pygame.tick() / 1000 # convert delta time (dt) from milliseconds to seconds, save into dt
 
 if __name__ == "__main__":
     main()
