0db17d7b59f8f09d96e208dab305800cb9cbab21
SweatyBytes <maxvheiningen@gmail.com>
2024-10-13 23:09:06 +0200

    added circleshape.py, player.py and updated main.py to draw a player

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import player
 from constants import *
 
 def main():
@@ -13,11 +14,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Instantiate the Player at the center of the screen
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player_instance = player.Player(player_x, player_y)
+
     clock = pygame.time.Clock() # Create an instance of Clock
     dt = 0
 
-    while 1 == 1:
+    while 1:
         screen.fill((0, 0, 0)) # Fill the screen with black
+
+        # Call the player's draw method
+        player_instance.draw(screen)
+
         pygame.display.flip() # Update the full display Surface to the screen
 
         for event in pygame.event.get(): # Iterate over all events in the pygame event queue
@@ -25,6 +35,8 @@ def main():
                 return # Exit the main game loop, effectively closing the window
         
         dt = clock.tick(60) / 1000 # convert delta time (dt) from milliseconds to seconds, save into dt
+
+
 
 if __name__ == "__main__":
     main()
